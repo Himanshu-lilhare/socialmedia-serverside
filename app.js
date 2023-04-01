b766a9784776fc984642aa8fba68f86c8f50459a
Raj <rajlilhare200@gmail.com>
2023-04-01 05:52:03 -0700

    changed order in app.js

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-import  express  from "express";
+import express from "express";
 import { config } from "dotenv";
 import userRoute from "./routes/user.js";
-import chatRouter  from "./routes/chat.js";
+import chatRouter from "./routes/chat.js";
 import { messageRouter } from "./routes/messages.js";
 import { errorMiddlewarebro } from "./middleware/errorMiddleware.js";
 import postRouter from "./routes/post.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
+config({ path: "./config/.env" });
 
-config({path:"./config/.env"})
+const app = express();
 
-const app=express()
-app.use(cookieParser())
-const router=express.Router()
-app.get('/',(req,res)=>{
-    res.send("hello bro welcome...dgdgf")
-})
-app.use(cors({
-    origin:'https://social-media-frontend-omega.vercel.app',
-    credentials:true,
-    method:["GET","POST","PUT","DELETE"]
-}))
-app.use(express.json({limit:'50mb'}))
-app.use(express.urlencoded({
-    limit:'50mb',
-    extended:true
-}))
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: true,
+  })
+);
 
+app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: "https://social-media-frontend-omega.vercel.app",
+    credentials: true,
+    method: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
-app.use("",userRoute)
-app.use("",postRouter)
-app.use('',chatRouter)
-app.use('',messageRouter)
-export default app
-app.use(errorMiddlewarebro)
+app.get("/", (req, res) => {
+  res.send("hello bro welcome...dgdgf");
+});
+
+app.use("", userRoute);
+app.use("", postRouter);
+app.use("", chatRouter);
+app.use("", messageRouter);
+export default app;
+app.use(errorMiddlewarebro);
